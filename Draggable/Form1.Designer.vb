edc9b445d0d0d4a8c462dbcb5abccda19fd37e24
Carm01 <6567665+Carm01@users.noreply.github.com>
2023-11-17 23:43:32 -0500

    Add project files.

---
+++ Draggable/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim CBlendItems1 As CButtonLib.cBlendItems = New CButtonLib.cBlendItems()
+        Dim CBlendItems2 As CButtonLib.cBlendItems = New CButtonLib.cBlendItems()
+        Me.PanelContainer = New System.Windows.Forms.Panel()
+        Me.btnStart = New CButtonLib.CButton()
+        Me.PanelTitleBar = New System.Windows.Forms.Panel()
+        Me.btnClose = New CButtonLib.CButton()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.PanelContainer.SuspendLayout()
+        Me.PanelTitleBar.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PanelContainer
+        '
+        Me.PanelContainer.BackColor = System.Drawing.Color.FromArgb(CType(CType(24, Byte), Integer), CType(CType(24, Byte), Integer), CType(CType(24, Byte), Integer))
+        Me.PanelContainer.Controls.Add(Me.btnStart)
+        Me.PanelContainer.Controls.Add(Me.PanelTitleBar)
+        Me.PanelContainer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelContainer.Location = New System.Drawing.Point(0, 0)
+        Me.PanelContainer.Name = "PanelContainer"
+        Me.PanelContainer.Size = New System.Drawing.Size(600, 450)
+        Me.PanelContainer.TabIndex = 0
+        '
+        'btnStart
+        '
+        Me.btnStart.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnStart.BorderColor = System.Drawing.Color.Black
+        CBlendItems1.iColor = New System.Drawing.Color() {System.Drawing.Color.FromArgb(CType(CType(24, Byte), Integer), CType(CType(24, Byte), Integer), CType(CType(24, Byte), Integer)), System.Drawing.Color.FromArgb(CType(CType(204, Byte), Integer), CType(CType(204, Byte), Integer), CType(CType(204, Byte), Integer)), System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))}
+        CBlendItems1.iPoint = New Single() {0!, 0.5!, 1.0!}
+        Me.btnStart.ColorFillBlend = CBlendItems1
+        Me.btnStart.ColorFillSolid = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnStart.Corners.All = 15
+        Me.btnStart.Corners.LowerLeft = 15
+        Me.btnStart.Corners.LowerRight = 15
+        Me.btnStart.Corners.UpperLeft = 15
+        Me.btnStart.Corners.UpperRight = 15
+        Me.btnStart.DesignerSelected = True
+        Me.btnStart.FillType = CButtonLib.CButton.eFillType.GradientPath
+        Me.btnStart.Font = New System.Drawing.Font("Century Gothic", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.ForeColor = System.Drawing.Color.Black
+        Me.btnStart.ImageIndex = 0
+        Me.btnStart.Location = New System.Drawing.Point(11, 407)
+        Me.btnStart.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(72, 32)
+        Me.btnStart.TabIndex = 2
+        Me.btnStart.Text = "Start"
+        Me.btnStart.TextShadow = System.Drawing.Color.Transparent
+        '
+        'PanelTitleBar
+        '
+        Me.PanelTitleBar.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.PanelTitleBar.Controls.Add(Me.btnClose)
+        Me.PanelTitleBar.Controls.Add(Me.lblTitle)
+        Me.PanelTitleBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelTitleBar.Location = New System.Drawing.Point(0, 0)
+        Me.PanelTitleBar.Name = "PanelTitleBar"
+        Me.PanelTitleBar.Size = New System.Drawing.Size(600, 50)
+        Me.PanelTitleBar.TabIndex = 0
+        '
+        'btnClose
+        '
+        Me.btnClose.BorderColor = System.Drawing.Color.Black
+        CBlendItems2.iColor = New System.Drawing.Color() {System.Drawing.Color.DarkSlateGray, System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(217, Byte), Integer), CType(CType(217, Byte), Integer)), System.Drawing.Color.FromArgb(CType(CType(223, Byte), Integer), CType(CType(236, Byte), Integer), CType(CType(236, Byte), Integer))}
+        CBlendItems2.iPoint = New Single() {0!, 0.5!, 1.0!}
+        Me.btnClose.ColorFillBlend = CBlendItems2
+        Me.btnClose.Corners.All = 15
+        Me.btnClose.Corners.LowerLeft = 15
+        Me.btnClose.Corners.LowerRight = 15
+        Me.btnClose.Corners.UpperLeft = 15
+        Me.btnClose.Corners.UpperRight = 15
+        Me.btnClose.DesignerSelected = False
+        Me.btnClose.FillType = CButtonLib.CButton.eFillType.GradientPath
+        Me.btnClose.Font = New System.Drawing.Font("Century Gothic", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClose.ForeColor = System.Drawing.Color.Black
+        Me.btnClose.ImageIndex = 0
+        Me.btnClose.Location = New System.Drawing.Point(515, 9)
+        Me.btnClose.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(72, 32)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Close"
+        Me.btnClose.TextShadow = System.Drawing.Color.Transparent
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Malgun Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(18, 16)
+        Me.lblTitle.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(116, 17)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Title and Version"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lime
+        Me.ClientSize = New System.Drawing.Size(600, 450)
+        Me.Controls.Add(Me.PanelContainer)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.PanelContainer.ResumeLayout(False)
+        Me.PanelTitleBar.ResumeLayout(False)
+        Me.PanelTitleBar.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelContainer As Panel
+    Friend WithEvents PanelTitleBar As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnClose As CButtonLib.CButton
+    Friend WithEvents btnStart As CButtonLib.CButton
+End Class
---
+++ Draggable/Form1.vb
@@ -0,0 +1,192 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Runtime.InteropServices
+' https://www.youtube.com/watch?v=Niu1w4CEIC4
+' https://rjcodeadvance.com/crear-formulario-de-bordes-redondeados-suaves-c-winforms/
+' Buttons
+' https://www.codeproject.com/Articles/26622/Custom-Button-Control-with-Gradient-Colors-and-Ext
+' .NET color names and values
+' https://www.99colors.net/dot-net-colors
+' https://cloford.com/resources/colours/500col.htm
+Public Class Form1
+
+    'Fields
+    Private borderRadius As Integer = 20
+    Private borderSize As Integer = 5
+    Private borderColor As Color = Color.DarkSlateGray
+
+    'Constructor
+    Public Sub New()
+            ' This call is required by the designer.
+            InitializeComponent()
+            ' Add any initialization after the InitializeComponent() call.
+            Me.FormBorderStyle = FormBorderStyle.None
+            Me.Padding = New Padding(borderSize)
+            Me.PanelTitleBar.BackColor = borderColor
+            Me.BackColor = borderColor
+        End Sub
+
+    'Drag Form
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+
+    Private Sub panelTitleBar_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseDown, PanelTitleBar.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112, &HF012, 0)
+    End Sub
+
+    'Minimize border-less form from task-bar
+    Protected Overrides ReadOnly Property CreateParams As CreateParams
+        Get
+            Dim cp As CreateParams = MyBase.CreateParams
+            cp.Style = cp.Style Or &H20000 '<--- Minimize border-less form from task-bar
+            Return cp
+        End Get
+    End Property
+
+    Private Function GetRoundedPath(rect As Rectangle, radius As Single) As GraphicsPath
+        Dim path As GraphicsPath = New GraphicsPath()
+        Dim curveSize As Single = radius * 2.0F
+        path.StartFigure()
+        path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90)
+        path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90)
+        path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90)
+        path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90)
+        path.CloseFigure()
+        Return path
+    End Function
+
+    Private Sub FormRegionAndBorder(form As Form, radius As Single, graph As Graphics, borderColor As Color, borderSize As Single)
+        If Me.WindowState <> FormWindowState.Minimized Then
+            Using roundPath As GraphicsPath = GetRoundedPath(form.ClientRectangle, radius)
+                Using penBorder As Pen = New Pen(borderColor, borderSize)
+                    Using transform As Matrix = New Matrix()
+
+                        graph.SmoothingMode = SmoothingMode.AntiAlias
+                        form.Region = New Region(roundPath)
+                        If borderSize >= 1 Then
+                            Dim rect As Rectangle = form.ClientRectangle
+                            Dim scaleX As Single = 1.0F - ((borderSize + 1) / rect.Width)
+                            Dim scaleY As Single = 1.0F - ((borderSize + 1) / rect.Height)
+                            transform.Scale(scaleX, scaleY)
+                            transform.Translate(borderSize / 1.6F, borderSize / 1.6F)
+                            graph.Transform = transform
+                            graph.DrawPath(penBorder, roundPath)
+                        End If
+
+                    End Using
+                End Using
+            End Using
+        End If
+    End Sub
+
+    Private Sub ControlRegionAndBorder(control As Control, radius As Single, graph As Graphics, borderColor As Color)
+        Using roundPath As GraphicsPath = GetRoundedPath(control.ClientRectangle, radius)
+
+            Using penBorder As Pen = New Pen(borderColor, 1)
+                graph.SmoothingMode = SmoothingMode.AntiAlias
+                control.Region = New Region(roundPath)
+                graph.DrawPath(penBorder, roundPath)
+            End Using
+        End Using
+    End Sub
+
+    ' this was creating weird edges over backgrounds, so i removed it from the original.
+    Private Sub DrawPath(rectForm As Rectangle, graphics As Graphics, color As Color)
+        'Using roundPath As GraphicsPath = GetRoundedPath(rectForm, borderRadius)
+        '    Using penBorder As Pen = New Pen(color, 3)
+        '        graphics.DrawPath(penBorder, roundPath)
+        '    End Using
+        'End Using
+    End Sub
+
+    Private Structure FormBoundsColors
+        Public TopLeftColor As Color
+        Public TopRightColor As Color
+        Public BottomLeftColor As Color
+        Public BottomRightColor As Color
+    End Structure
+    Private Function GetFormBoundsColors() As FormBoundsColors
+        Dim fbColor = New FormBoundsColors()
+        Using bmp = New Bitmap(1, 1)
+            Using graph As Graphics = Graphics.FromImage(bmp)
+                Dim rectBmp As New Rectangle(0, 0, 1, 1)
+                'Top Left
+                rectBmp.X = Me.Bounds.X - 1
+                rectBmp.Y = Me.Bounds.Y
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size)
+                fbColor.TopLeftColor = bmp.GetPixel(0, 0)
+                'Top Right
+                rectBmp.X = Me.Bounds.Right
+                rectBmp.Y = Me.Bounds.Y
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size)
+                fbColor.TopRightColor = bmp.GetPixel(0, 0)
+                'Bottom Left
+                rectBmp.X = Me.Bounds.X
+                rectBmp.Y = Me.Bounds.Bottom
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size)
+                fbColor.BottomLeftColor = bmp.GetPixel(0, 0)
+                'Bottom Right
+                rectBmp.X = Me.Bounds.Right
+                rectBmp.Y = Me.Bounds.Bottom
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size)
+                fbColor.BottomRightColor = bmp.GetPixel(0, 0)
+            End Using
+        End Using
+        Return fbColor
+    End Function
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        '-> Smooth Outer Border
+        e.Graphics.SmoothingMode = SmoothingMode.AntiAlias
+        Dim rectForm As Rectangle = Me.ClientRectangle
+        Dim mWidht As Integer = rectForm.Width / 2
+        Dim mHeight As Integer = rectForm.Height / 2
+        Dim fbColor = GetFormBoundsColors()
+
+        'Top Left
+        DrawPath(rectForm, e.Graphics, fbColor.TopLeftColor)
+
+        'Top Right
+        Dim rectTopRight As New Rectangle(rectForm.Right - mWidht, rectForm.Y, mWidht, mHeight)
+        DrawPath(rectTopRight, e.Graphics, fbColor.TopRightColor)
+
+        'Bottom Left
+        Dim rectBottomLeft As New Rectangle(rectForm.X, rectForm.Bottom - mHeight, mWidht, mHeight)
+        DrawPath(rectBottomLeft, e.Graphics, fbColor.BottomLeftColor)
+
+        'Bottom Right
+        Dim rectBottomRight As New Rectangle(rectForm.Right - mWidht, rectForm.Bottom - mHeight, mWidht, mHeight)
+        DrawPath(rectBottomRight, e.Graphics, fbColor.BottomRightColor)
+
+        '-> Set Rounded Region and Border
+        FormRegionAndBorder(Me, borderRadius, e.Graphics, borderColor, borderSize)
+    End Sub
+
+    Private Sub PanelContainer_Paint(sender As Object, e As PaintEventArgs) Handles PanelContainer.Paint
+        ControlRegionAndBorder(PanelContainer, borderRadius - (borderSize / 2.0F), e.Graphics, borderColor)
+    End Sub
+
+    Private Sub Form1_ResizeEnd(sender As Object, e As EventArgs) Handles MyBase.ResizeEnd
+        Me.Invalidate()
+    End Sub
+
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+        Me.Invalidate()
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        Me.Invalidate()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lblTitle.Text = "Application v." & Application.ProductVersion
+        Me.CenterToScreen()
+    End Sub
+    Private Sub btnClose_ClickButtonArea(Sender As Object, e As MouseEventArgs) Handles btnClose.ClickButtonArea
+        Application.Exit()
+    End Sub
+End Class
